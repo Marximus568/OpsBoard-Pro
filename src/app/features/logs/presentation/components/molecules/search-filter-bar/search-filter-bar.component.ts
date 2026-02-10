d5d5d38971243cab0d16c97c7b9dc1c111193727
Marximus568 <marximusleal568@gmail.com>
2026-02-10 12:38:18 -0500

    fix: code duplicate..

--- src/app/features/logs/presentation/components/molecules/search-filter-bar/search-filter-bar.component.ts
+++ src/app/features/logs/presentation/components/molecules/search-filter-bar/search-filter-bar.component.ts
@@ -27,13 +27,6 @@ export class SearchFilterBarComponent {
     }
 
     onExport(): void {
-        const content = this.facade.exportLogs();
-        const blob = new Blob([content], { type: 'text/plain' });
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = `logs-export-${new Date().getTime()}.log`;
-        a.click();
-        window.URL.revokeObjectURL(url);
+        this.facade.exportLogs();
     }
 }
